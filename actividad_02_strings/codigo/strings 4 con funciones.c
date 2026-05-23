732f4a8c20702ff8c7ec17ac0f9ff77049bcd1de
Diegozarher27 <diego.zaragoza9239@alumnos.udg.mx>
2026-05-23 11:34:42 -0600

    Update strings 4

--- actividad_02_strings/codigo/strings 4 con funciones.c
+++ actividad_02_strings/codigo/strings 4 con funciones.c
@@ -1,81 +1,34 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main(){
-
-    char alumno [50] = "ANTONIO";
-    char alumno2 [50];
-    char lista [4][50];
-    char apellido[50] = "perez";
-    int i;
-
-    printf("%s", alumno);
-    printf("\n");
-
-    printf("Dame tu nombre alumno \n");
-
-    // EJEMPLO CON SCANF (solo lee una palabra)
-    scanf("%s", alumno2);
-    printf("Tu nombre con scanf es: %s\n", alumno2);
-
-    getchar(); // limpiar buffer para poder usar fgets
-
-    // EJEMPLO CON FGETS (permite espacios)
-    printf("Ahora escribe tu nombre completo:\n");
-    fgets(alumno2,sizeof(alumno2),stdin);
-    printf("Tu nombre con fgets es: %s\n",alumno2);
-
-    printf("////EQUIPO CHIVAS////\n");
-
-    // EJEMPLO CON STRCPY
-    strcpy(lista[0], "Angel");
-    strcpy(lista[1], "Diego");
-    strcpy(lista[2], "Carlos");
-    strcpy(lista[3], "Jorge");
-
-    printf("Lista original:\n");
-    for (i=0;i<4;i++){
-        printf("%s\n",lista[i]);
+int main()
+{
+    int tablero[5][5];
+    char nombre[20];
+    for(int i=0; i<5;i++){
+        for(int j=0;j<5;j++){
+            tablero[i][j]= '~';
+            tablero[1][2]= 'B';
+            tablero[3][4]= 'X';
+            tablero[2][1]= 'O';
+           printf("(%c)", tablero[i][j]);
+        }
+        printf("\n");
     }
-
-    printf("\nAhora puedes cambiar los nombres del equipo\n");
-
-    // LECTURA CON FGETS
-    for (i=0;i<4;i++){
-        printf("Jugador %d: ", i+1);
-        fgets(lista[i],sizeof(lista[i]),stdin);
-        if(lista[i][strlen(lista[i]) - 1] == '\n'){
-    lista[i][strlen(lista[i]) - 1] = '\0';
-    }
-    }
-
-    printf("\nLista final del equipo:\n");
-
-    for(i=0;i<4;i++){
-        printf("%s\n",lista[i]);
-    }
-
-    printf("\n");
-
-    // EJEMPLO DE STRLEN
-    printf("Numero de caracteres del jugador 1: %d\n",strlen(lista[0]));
-    printf("Numero de caracteres del jugador 2: %d\n",strlen(lista[1]));
-    printf("Numero de caracteres del jugador 3: %d\n",strlen(lista[2]));
-    printf("Numero de caracteres del jugador 4: %d\n",strlen(lista[3]));
-
-    // EJEMPLO DE STRCAT
-    printf("\nEjemplo de concatenacion\n");
-    strcat(alumno, apellido);
-    printf("Nombre concatenado: %s\n", alumno);
-
-    // EJEMPLO DE STRCMP
-    printf("\nComparacion de nombres\n");
-
-    if(strcmp(lista[0], lista[1]) == 0){
-        printf("El jugador 1 y jugador 2 tienen el mismo nombre\n");
-    }else{
-        printf("Los nombres del jugador 1 y jugador 2 son diferentes\n");
-    }
-
-    return 0;
+    FILE *archivo;
+    printf("dame tu nombre\n");
+    fgets(nombre,sizeof(nombre),stdin);
+    archivo = fopen("tablero.txt","w");
+        for(int i=0; i<5;i++){
+            for(int j=0;j<5;j++){
+                fprintf(archivo, "(%c)", tablero[i][j]);
+            }
+            fprintf(archivo, "\n");
+        }
+            fprintf(archivo, "diego zaragoza\n");
+            fclose(archivo);
+            printf("cerrando el archivo...");
+            archivo = fopen("tablero.txt","r");
+            printf("archivo abierto en modo de lectura..");
+            return 0;
 }
